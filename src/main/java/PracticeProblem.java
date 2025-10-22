304889d89ea1ee33e545778450c6366fd502c2fa
Shaoyang Chen <shaoyang.chen@student.tdsb.on.ca>
2025-10-22 14:59:36 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,20 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int sumOfDigits(int x){
+		int sum = 0;
+		x = Math.abs(x);
+		while(x > 0){
+			sum = sum + x % 10;
+			x = x / 10;
+		}
+		return sum;
+	}
+
+	public static int factorial(int inputNum) {
+		if (inputNum == 0) return 1;
+		if (inputNum < 0) return 0;
+		return inputNum * factorial(inputNum - 1);
+	}
 
 }
